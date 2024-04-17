6e1f704ac085781a7c6e1e9cbb8e85a197c2e3cb
Andrea <gelsogrove@gmail.com>
2024-04-17 19:51:43 +0200

    onnxx finish

--- 6_detectONNX.py
+++ 6_detectONNX.py
@@ -5,36 +5,42 @@ import onnxruntime as rt
 sess = rt.InferenceSession('yolov5/runs/train/exp/weights/best.onnx')
 
 # Impostazioni di rilevamento
-confidence_threshold = 0.7  # Soglia di confidenza aggiornata
+confidence_threshold = 0.6 # Soglia di confidenza aggiornata
 
-# Funzione per trovare la posizione del box con la probabilità più alta e disegnare un cerchio in quella posizione
-def draw_highest_probability_circle(frame, detections, frame_width, frame_height):
-    max_confidence = 0
-    max_confidence_position = None
-
+# Funzione per trovare la posizione del box con la probabilità più alta e disegnare un rettangolo in quella posizione
+def draw_highest_probability_rectangle(frame, detections, frame_width, frame_height):
     for detection in detections:
+        max_confidence = 0
+        
+        # Trova la probabilità più alta tra tutti gli oggetti rilevati
         for obj in detection:
-            # Estrai il valore di confidenza
             confidence = obj[4]
-
-            # Verifica se la confidenza supera la soglia e se è maggiore della massima finora
-            if confidence > confidence_threshold and confidence > max_confidence:
+            if confidence > max_confidence:
                 max_confidence = confidence
-                startX_rel, startY_rel, _, _ = obj[0:4]
 
-                # Converti le coordinate relative alle dimensioni del blob alle dimensioni effettive del frame
-                startX = int(startX_rel * frame_width / 640)
-                startY = int(startY_rel * frame_height / 480)
+        # Disegna un rettangolo solo per gli oggetti con la probabilità più alta
+        for obj in detection:
+            confidence = obj[4]
+            if confidence == max_confidence and confidence > confidence_threshold:
+                centerX = int(obj[0] * frame_width /640)
+                centerY = int(obj[1] * frame_height /480)
+                width = int(obj[2] * frame_width /640)
+                height = int(obj[3] * frame_height /480)
 
-                max_confidence_position = (startX, startY)
+                startX = int(centerX - width / 2)
+                startY = int(centerY - height / 2)
 
-    if max_confidence_position is not None:
-        cv2.circle(frame, max_confidence_position, 15, (255, 255, 255), -1)
+                endX = startX + width
+                endY = startY + height
+
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 255), 2)
 
 # Funzione per rilevare gli oggetti in un'immagine dalla webcam
 def detect_objects_webcam():
     # Attiva la webcam
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     while True:
         # Leggi il frame dalla webcam
@@ -46,13 +52,13 @@ def detect_objects_webcam():
         frame_height, frame_width = frame.shape[:2]
 
         # Prepara il frame per il rilevamento
-        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (640, 480), swapRB=True, crop=False)
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (640, 480)), 1/255.0, (640, 480), swapRB=True, crop=False)
 
         # Esegui il rilevamento
         detections = sess.run(None, {'images': blob})[0]
 
-        # Disegna un cerchio nella posizione del box con la probabilità più alta
-        draw_highest_probability_circle(frame, detections, frame_width, frame_height)
+        # Disegna un rettangolo intorno agli oggetti rilevati con alta probabilità
+        draw_highest_probability_rectangle(frame, detections, frame_width, frame_height)
 
         # Mostra il frame con i rilevamenti
         cv2.imshow('Rilevamenti', frame)
